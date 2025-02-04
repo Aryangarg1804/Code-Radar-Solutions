290a7034f55590ba171867e584ffad2035ce6096
Aryan <aryan986853@gmail.com>
2025-02-05 01:35:20 +0530

    Solution to Count the Number of Palindromic Numbers in an Array

--- Count the Number of Palindromic Numbers in an Array@@676c02002dbc8c3e7a280b89/code.c
+++ Count the Number of Palindromic Numbers in an Array@@676c02002dbc8c3e7a280b89/code.c
@@ -3,7 +3,7 @@
 int main() {
     int a;
     scanf("%d", &a);
-    int b[a];  
+    int b[a];  // Using VLA (assuming valid in your environment)
 
     for (int i = 0; i < a; i++) {
         scanf("%d", &b[i]);
@@ -11,7 +11,7 @@ int main() {
 
     int x = 0;
     for (int i = 0; i < a; i++) {
-        int y = (b[i] < 0) ? -b[i] : b[i];  
+        int y = (b[i] < 0) ? -b[i] : b[i];  // Take absolute value without modifying original array
         int z = 0, temp = y;
 
         while (temp != 0) {
