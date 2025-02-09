f4580f06b8425ccec82afc7734b17ba98f2987b9
Aryan <aryan986853@gmail.com>
2025-02-09 20:59:30 +0530

    Solution to Flip Bits of a Binary Number

--- Flip Bits of a Binary Number@@67882a731b4392d8fc1577f3/code.c
+++ Flip Bits of a Binary Number@@67882a731b4392d8fc1577f3/code.c
@@ -1,15 +1,7 @@
-#include <stdio.h>
-
-int countTrailingZeros(int N) {
-    if (N == 0)
-        return 32; // Special case: all bits are 0
-    
-    return __builtin_ctz(N); // Built-in function to count trailing zeros
-}
-
-int main() {
-    int N;
-    scanf("%d", &N);
-    printf("%d\n", countTrailingZeros(N));
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",%a);
+    printf("%d",~a);
     return 0;
 }
