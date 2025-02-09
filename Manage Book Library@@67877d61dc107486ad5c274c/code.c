228bc967ff40091a717386752e5b34e7b0944e72
Aryan <aryan986853@gmail.com>
2025-02-10 00:11:26 +0530

    Solution to Manage Book Library

--- Manage Book Library@@67877d61dc107486ad5c274c/code.c
+++ Manage Book Library@@67877d61dc107486ad5c274c/code.c
@@ -17,7 +17,7 @@ int main() {
     scanf("%f",&a);
     for(int i=0;i<n;i++){
         if(b[i].price>=a){
-            printf("Title: %s,Author: %s, Price: %.2f",b[i].title,b[i].author,b[i].price);
+            printf("Title: %s, Author: %s, Price: %.2f\n",b[i].title,b[i].author,b[i].price);
         }
     }
 
