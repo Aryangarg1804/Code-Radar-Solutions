922709106b1eb3197e4f7839ffdae4d0f2f2f87e
Aryan <aryan986853@gmail.com>
2025-02-09 23:51:38 +0530

    Solution to Sort Students by Marks

--- Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -17,14 +17,11 @@ int main() {
     for(int i=0;i<n-1;i++){
         for(int j=i+1;j<n;j++){
             if(a[i]<=a[j]){
-                int temp = a[i];
+                float temp = a[i];
                 a[i] = a[j];
                 a[j] = temp;
             }
         }
-    }
-    for(int i=0;i<n;i++){
-        printf("%f  %f \n",s[i].marks,a[i]);
     }
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
